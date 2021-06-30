cd97b07214efa60cfe1401bb2b8897bec52c94a7
FANTAZER <kuznecovs45@gmail.com>
2021-06-30 20:08:02 +0300

    add lpTemplate

--- module/advant.php
+++ module/advant.php
@@ -1,8 +1,63 @@
+<div class="section section-advance" id="advance">
+	<div class="main-cont">
+		<div class="section-title">Почему выбирают нас</div>
+		<div class="advance">
+			<div class="advance-el">
+				<div class="advance-icon">
+					<svg class="icon">
+						<use xlink:href="#attest"></use>
+					</svg>
+				</div>
+				<div class="advance-text">Штат аттестованных кадастровых инженеров. Лицензии СРО.</div>
+			</div>
+			<div class="advance-el">
+				<div class="advance-icon">
+					<svg class="icon">
+						<use xlink:href="#time"></use>
+					</svg>
+				</div>
+				<div class="advance-text">Оперативность решения вопросов.</div>
+			</div>
+			<div class="advance-el">
+				<div class="advance-icon">
+					<svg class="icon">
+						<use xlink:href="#worker"></use>
+					</svg>
+				</div>
+				<div class="advance-text">Все работы выполняются нашими штатными специалистами</div>
+			</div>
+			<div class="advance-el">
+				<div class="advance-icon">
+					<svg class="icon">
+						<use xlink:href="#money"></use>
+					</svg>
+				</div>
+				<div class="advance-text">Окончательный расчет производится по факту выполнения работ.</div>
+			</div>
+			<div class="advance-el">
+				<div class="advance-icon">
+					<svg class="icon">
+						<use xlink:href="#posred"></use>
+					</svg>
+				</div>
+				<div class="advance-text">Мы работаем без посредников, поэтому Вы экономите свои средства и время.</div>
+			</div>
+			<div class="advance-el">
+				<div class="advance-icon">
+					<svg class="icon">
+						<use xlink:href="#result"></use>
+					</svg>
+				</div>
+				<div class="advance-text">Более 5000 выполненных объектов по Москве и Московской области.</div>
+			</div>
+		</div>
+	</div>
+</div>
 <div class="article-advant-wrap">
 	<div class="main-cont">
 		<div class="article-advant">
 			<div class="article-advant__info">
-				<div class="article-advant__title">Победа в Мосгорсуде у города Москвы! </div>
+				<div class="article-advant__title">Победа в Мосгорсуде у города Москвы!</div>
 				<div class="article-advant__text">Центр геодезических и кадастровых услуг поздравляет жителей многоквартирного дома, расположенного по адресу: г. Москва, ул. Цандера, д. 7 с тем, что они выиграли в Мосгорсуде у города Москвы право на отмену строительства 22-этажного дома по программе реновации!</div>
 				<a href="/pobeda-mosgorsude-u-goroda-moskvy/" class="btn--lg btn--mark article-advant__more">Подробнее</a>
 			</div>
@@ -12,58 +67,3 @@
 		</div>
 	</div>
 </div>
-<div class="section section-advance" id="advance">
-			<div class="main-cont">
-				<div class="section-title">Почему выбирают нас</div>
-				<div class="advance">
-					<div class="advance-el">
-						<div class="advance-icon">
-							<svg class="icon">
-								<use xlink:href="#attest"></use>
-							</svg>
-						</div>
-						<div class="advance-text">Штат аттестованных кадастровых инженеров. Лицензии СРО.</div>
-					</div>
-					<div class="advance-el">
-						<div class="advance-icon">
-							<svg class="icon">
-								<use xlink:href="#time"></use>
-							</svg>
-						</div>
-						<div class="advance-text">Оперативность решения вопросов.</div>
-					</div>
-					<div class="advance-el">
-						<div class="advance-icon">
-							<svg class="icon">
-								<use xlink:href="#worker"></use>
-							</svg>
-						</div>
-						<div class="advance-text">Все работы выполняются нашими штатными специалистами</div>
-					</div>
-					<div class="advance-el">
-						<div class="advance-icon">
-							<svg class="icon">
-								<use xlink:href="#money"></use>
-							</svg>
-						</div>
-						<div class="advance-text">Окончательный расчет производится по факту выполнения работ.</div>
-					</div>
-					<div class="advance-el">
-						<div class="advance-icon">
-							<svg class="icon">
-								<use xlink:href="#posred"></use>
-							</svg>
-						</div>
-						<div class="advance-text">Мы работаем без посредников, поэтому Вы экономите свои средства и время.</div>
-					</div>
-					<div class="advance-el">
-						<div class="advance-icon">
-							<svg class="icon">
-								<use xlink:href="#result"></use>
-							</svg>
-						</div>
-						<div class="advance-text">Более 5000 выполненных объектов по Москве и Московской области.</div>
-					</div>
-				</div>
-			</div>
-		</div>
--- single-serviceTemplate.php
+++ single-serviceTemplate.php
@@ -6,57 +6,166 @@
 ?>
 <? get_header('lp'); ?>
 
-<div class="main-cont">
+<!--mainBanner-->
+<div class="lp-baner lp-baner--borderland lp-baner--dark">
+	<div class="lp-baner__img lp-baner__img--top" style="background-image:url('<?= get_field('mainbannerimg');?>');"></div>
+	<div class="lp-baner-filter--green"></div>
+	<div class="main-cont">
+		<div class="lp-baner__wrap">
+			<div class="lp-baner__content">
+				<div class="lp-baner__title lp-baner__title--sm"><?= the_title(); ?></div>
+				<div class="lp-baner__title-sub"><?= get_field('second-title'); ?></div>
+				<div class="header__get">
+					<a class="header__btn header__btn-more" href="#why">Подробнее</a>
+					<div class="header__btn header__btn-get header__btn-get--yellow modal-get" data-modal="order">Заказать</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--mainBanner === end-->
 
-	<h1><?= the_title(); ?></h1>
+<!--howWork-->
+<div class="section section--doc section--fill" style="background-image:url('<?php echo get_template_directory_uri(); ?>/img/igs/concrete-texture.png');">
+	<div class="main-cont-sm">
+		<div class="section-title">Как это работает</div>
+		<div class="text-block text-block--seal ">
+			<div class="seal">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/seal.png" alt=""/>
+			</div>
+      <?= get_field('howwork'); ?>
+      <?php if( get_field('howworkaddtext')){ ?>
+			<dic class="f f-jcc">
+				<div class="btn--lg btn--mark modal-get" data-modal="howWorkAddText">Узнать подробнее</div>
+			</dic>
+			<? } ?>
+		</div>
+	</div>
+</div>
+<!--howWork === end-->
 
-	<hr class="mb-32">
-	<h2>Описание</h2>
-	<p><?= get_field('second-title'); ?></p>
+<!--whyNeed-->
+<div class="section section--why section--union section--mobile" id="why" style="background-image:url('<?= get_field('whyneedimg');?>');">
+	<div class="main-cont">
+		<div class="section-wrap">
+			<div class="section-call section-call--mid">
+				<div class="text-block">
+					<div class="section-title section-title--left">Зачем нужно</div>
+					<div class="box">
+            <?= get_field('whyNeed'); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--whyNeed === end-->
 
-	<hr class="mb-32">
-	<h2>Зачем нужно</h2>
-  <?= get_field('whyNeed'); ?>
+<!--doc-->
+<div class="section section--doc">
+	<div class="main-cont">
+		<div class="section-title">Какие нужны документы?</div>
+		<div class="section-title-sub">Максимальный перечень необходимых документов</div>
+		<div class="g g-2 sm__g-1 gap-20">
+      <? $docNumber = 0; ?>
+      <? $docNumberIconArray = ['attest', 'plan', 'doc', 'planning','posred','idea','image','information']; ?>
+      <? while (have_rows('needDoc')): the_row(); ?>
+				<div class="bor bor--graySnow f f-col p-24 text br-12">
+						<svg class="icon--mark mb-24 icon--xl">
+							<use xlink:href="#<?= $docNumberIconArray[$docNumber]?>"></use>
+						</svg>
+						<div class="type--bold type--md type--black  mb-24"><?= the_sub_field('needDocTitle'); ?></div>
+						<div class=" type--black t mb-24 type--lh-1_4"><?= the_sub_field('needDocText'); ?></div>
+				</div>
+        <? $docNumber++; ?>
+      <? endwhile; ?>
+		</div>
+	</div>
+</div>
+<!--doc === end-->
 
-	<hr class="mb-32">
-	<h2>Как работает</h2>
-  <?= get_field('howwork'); ?>
+<!--cost-->
+<div class="section section--calc section--calc--topograf">
+	<div class="main-cont">
+		<div class="section-title">Стоимость наших работ</div>
+		<div class="section-title-sub">Работаем в Москве и во всех районах Московской области.</div>
+		<div class="calc">
+			<!--input(type="range" placeholder=""  class="calc-range--topograf")-->
+			<div class="calc-range__wrap">
+				<div class="calc-range-val calc-range-val--lg">
+					<div class="calc-range-numb"><span class="type--sm">от</span> <?= get_field('cost'); ?></div>
+					<div class="calc-range-pay"><sup>*</sup></div>
+				</div>
+			</div>
+			<div class="calc-text">*Цена услуги может меняться в зависимости от объемы выполненных работ/div>
+			</div>
+			<div class="get-sale-row">
+				<div class="get-sale__btn modal-get" data-modal="sale">Посмотреть все цены</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--cost === end-->
 
-	<hr class="mb-32">
-	<h2>Какие нужны документы</h2>
-  <? while (have_rows('needDoc')): the_row(); ?>
-		<p><b><? the_sub_field('needDocTitle'); ?></b> - <? the_sub_field('needDocText'); ?></p><br>
-  <? endwhile; ?>
 
-	<hr class="mb-32">
-	<h2>Цена услуги</h2>
-	<b><?= get_field('cost'); ?></b>
-
-	<hr class="mb-32">
-	<h2>Ссылка на прайс</h2>
-	<b><?= get_field('costlink'); ?></b>
-
-	<hr class="mb-32">
-	<h2>Что войдет в стоимость</h2>
-	<ol>
-    <? while (have_rows('costconsist')): the_row(); ?>
-			<li><? the_sub_field('costConsistEl'); ?></li>
-    <? endwhile; ?>
-	</ol>
-
-	<hr class="mb-32">
-	<h2>Что войдет в стоимость</h2>
-	<ol>
-    <? while (have_rows('stepwork')): the_row(); ?>
-			<li><? the_sub_field('stepworkel'); ?></li>
-    <? endwhile; ?>
-	</ol>
-
+<!--need payment-->
+<div class="section section--doc">
+	<div class="main-cont-sm">
+		<div class="section-title mb-32">Что войдет в стоимость работ ?</div>
+		<div class="g g-1 sm__g-1 gap-20">
+      <? $costStep = 0; ?>
+      <? $costStepIconArray = ['case', 'design', 'protractor', 'planning', 'agreement', 'topograf']; ?>
+      <? while (have_rows('costconsist')): the_row(); ?>
+				<div class="bor bor--graySnow f p-24 text br-12 gap-20 f-aic">
+						<svg class="icon--mark icon--xl">
+							<use xlink:href="#<?= $costStepIconArray[$costStep] ?>"></use>
+						</svg>
+					<div class="type--bold type--black type--lh-1_4"><?= the_sub_field('costConsistEl'); ?></div>
+				</div>
+        <? $costStep++; ?>
+      <? endwhile; ?>
+		</div>
+	</div>
 </div>
 
 
-<? include('module/advant.php'); ?>
+<!--step work-->
+<div class="section section-step" id="step">
+	<div class="section-filter"></div>
+	<div class="main-cont">
+		<div class="section-title">Как мы работаем</div>
+		<div class="step">
+      <? $stepNumber = 1; ?>
+      <? while (have_rows('stepwork')): the_row(); ?>
+				<div class="step-row <? if ($stepNumber % 2 === 0) {
+          echo "step-row--right";
+        } ?>">
+					<div class="step-el">
+            <? if ($stepNumber % 2 === 0) { ?>
+							<div class="step-line">
+								<div class="step-dot"></div>
+							</div>
+            <? } ?>
+						<div class="step-content">
+							<div class="step-numb">0<?= $stepNumber; ?></div>
+							<div class="step-text">
+								<div class="step-text__descr"><? the_sub_field('stepworkel'); ?></div>
+							</div>
+						</div>
+            <? if ($stepNumber % 2 != 0) { ?>
+							<div class="step-line">
+								<div class="step-dot"></div>
+							</div>
+            <? } ?>
+					</div>
+				</div>
+        <? $stepNumber++; ?>
+      <? endwhile; ?>
+		</div>
+	</div>
+</div>
 
+<? include('module/advant.php'); ?>
 <? include('module/customers.php'); ?>
 <? include('module/sertificate.php'); ?>
 <? include('module/contact-form.php'); ?>
